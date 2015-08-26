25e296bf80a0f3d99ac22a6ce9de4c8345b979ec
Luke Pacholski <luke.pacholski@gmail.com>
2015-08-26 09:20:05 -0700

    Updated to the details bar styling & logic.

--- stay-calm/details-bar.js
+++ stay-calm/details-bar.js
@@ -1,8 +1,18 @@
+var detailsBarHeight, detailsBar, bodyEl;
+
+$(document).on("ready",function(){
+    bodyEl = $("body");
+    detailsBar = $(".details-bar");
+    detailsBarHeight = 64;
+    var currentPadding = parseInt(bodyEl.css("padding-top"));
+    bodyEl.css("padding-top",currentPadding + detailsBarHeight);
+});
+
 $(window).on("scroll", function(e) {
     var scrollDistance = $(this).scrollTop();
-    if(scrollDistance >= 64) {
-        $("body").addClass("scrolled");
+    if(scrollDistance >= detailsBarHeight) {
+        detailsBar.addClass("scrolled");
     } else {
-        $("body").removeClass("scrolled");
+        detailsBar.removeClass("scrolled");
     }
 });
