f602ead6b599c9801ec5cf11afff0f481669843c
Luke Pacholski <luke.pacholski@gmail.com>
2015-09-15 15:20:17 -0700

    something or other

--- stay-calm-2/details-bar.js
+++ stay-calm-2/details-bar.js
@@ -1,31 +1,27 @@
 var detailsBarHeight, detailsBar, bodyEl, scrollDistance;
 
 $(document).on("ready",function(){
-    bodyEl = $("body");
-    detailsBar = $(".details-bar");
-    detailsBarHeight = 64;
+  bodyEl = $("body");
+  detailsBar = $(".details-bar");
+  detailsBarHeight = 64;
 
-    detailsBar.on("mouseover",function(){
-      if(scrollDistance >= detailsBarHeight){
-        $(this).removeClass("scrolled");
-      }
-    })
-
-    detailsBar.on("mouseout",function(){
-      if(scrollDistance >= detailsBarHeight){
-        $(this).addClass("scrolled");
-      }
-    })
-
+  detailsBar.on("mouseover",function(){
+    if(scrollDistance >= detailsBarHeight){
+      $(this).removeClass("scrolled");
+    }
+  })
+  detailsBar.on("mouseout",function(){
+    if(scrollDistance >= detailsBarHeight){
+      $(this).addClass("scrolled");
+    }
+  })
 });
 
-
-
 $(window).on("scroll", function(e) {
-    scrollDistance = $(this).scrollTop();
-    if(scrollDistance >= detailsBarHeight) {
-        detailsBar.addClass("scrolled");
-    } else {
-        detailsBar.removeClass("scrolled");
-    }
+  scrollDistance = $(this).scrollTop();
+  if(scrollDistance >= detailsBarHeight) {
+    detailsBar.addClass("scrolled");
+  } else {
+    detailsBar.removeClass("scrolled");
+  }
 });
