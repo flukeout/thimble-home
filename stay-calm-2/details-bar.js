a1009eb1174edd8b094f6b78604340198af28a5e
Luke Pacholski <luke.pacholski@gmail.com>
2015-09-30 11:33:52 -0500

    Updates to the details bar for published projects

--- stay-calm-2/details-bar.js
+++ stay-calm-2/details-bar.js
@@ -1,34 +1,58 @@
 var detailsBarHeight, detailsBar, bodyEl, scrollDistance;
+// var isTouchDevice = 'ontouchstart' in document.documentElement;
+var closeDelay = 275;
 
 $(document).on("ready",function(){
+
+
   bodyEl = $("body");
   detailsBar = $(".details-bar");
-  detailsBarHeight = 64;
 
-  detailsBar.on("mouseover",function(){
-    if(scrollDistance >= detailsBarHeight){
-      $(this).removeClass("scrolled");
-    }
-  });
+  if(isTouchDevice) {
 
-  detailsBar.on("mouseout",function(){
-    if(scrollDistance >= detailsBarHeight){
-      $(this).addClass("scrolled");
-    }
-  });
-
-  $(".mobile-icon").on("click",function(){
-    alert("Show popup with info");
-  });
+    detailsBar.addClass("touch-mode");
+    detailsBar.find(".thimble-logo").on("click",function(){
+      detailsBar.removeClass("collapsed");
+    });
+    detailsBar.find(".close-details-bar").on("click",function(){
+      detailsBar.addClass("collapsed");
+    });
 
 
-});
-
-$(window).on("scroll", function(e) {
-  scrollDistance = $(this).scrollTop();
-  if(scrollDistance >= detailsBarHeight) {
-    detailsBar.addClass("scrolled");
   } else {
-    detailsBar.removeClass("scrolled");
+
+    detailsBar.addClass("mouse-mode");
+
+    detailsBar.on("mouseenter",function(){
+      $(this).removeClass("collapsed");
+    });
+
+    detailsBar.on("mouseleave",function(){
+      setTimeout(function(){
+        closeBar();
+      },closeDelay);
+    });
+
   }
+
+
+  // $(".mobile-icon").on("click",function(){
+  //   showProjectInfo();
+  // });
+
 });
+
+function closeBar(){
+  detailsBar.addClass("collapsed");
+}
+
+// $(window).on("scroll", function(e) {
+  // scrollDistance = $(this).scrollTop();
+  // if(scrollDistance >= detailsBarHeight) {
+  //   detailsBar.addClass("scrolled");
+  // } else {
+  //   detailsBar.removeClass("scrolled");
+  // }
+// });
+
+
